558c3c985fdab0935367e6f18c3aa692285c732d
mguida22 <michael.guida@colorado.edu>
2015-08-27 00:47:29 -0600

    initial setup of express

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var api = require('./routes/api');
+
+var app = express();
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
+
+app.use('/api', api);
+
+app.get('/', function(req, res){
+  res.sendStatus(200);
+});
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+
+if (app.get('env') === 'development') {
+
+  app.use(function(err, req, res, next) {
+    res.sendStatus(err.status || 500);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.sendStatus(err.status || 500);
+});
+
+app.listen(3000);
+
+module.exports = app;
